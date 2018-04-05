3923c3aeb1fa9394dd6edda7e4a1d11ea3fe29bf
Tsvetan Tsvetanov <tsvetanov.19@gmail.com>
2018-04-05 17:31:39 +0300

    code format

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ JSON_TEMPLATE = {
 
 
 def get_channel_data(target_url):
-    call_data = []
+    talks = []
     lines_num = 0
     max_monologue = 0
     total_talk_time = 0
@@ -47,11 +47,11 @@ def get_channel_data(target_url):
         # calculate talk tuple [begin, end] for every second file line
         # update longest monologue if new maximum has been reached
         if lines_num % 2 == 0:
-            call_data.insert(int(lines_num / 2), [talk_start, talk_end])
+            talks.insert(int(lines_num / 2), [talk_start, talk_end])
             monologue_duration = talk_end - talk_start
             total_talk_time = total_talk_time + monologue_duration
             if (monologue_duration) > max_monologue:
-                max_monologue = round(monologue_duration,2)
+                max_monologue = round(monologue_duration, 2)
         lines_num = lines_num + 1
 
         # last line - save duration
@@ -61,17 +61,19 @@ def get_channel_data(target_url):
     return {
         "last_line": lines_num,
         "max_monologue": max_monologue,
-        "total_talk_time" : total_talk_time,
-        "talks": call_data,
+        "total_talk_time": total_talk_time,
+        "talks": talks,
         "duration": duration,
     }
 
 
 def get_channel_talk_percentage(total_talk_time, call_duration):
-     return round(total_talk_time*100/call_duration,2)
+    return round(total_talk_time * 100 / call_duration, 2)
+
 
 def get_talk_duration(customer_data, user_data):
     return max(customer_data["duration"], user_data["duration"])
+
 
 def populated_json(longest_user_monologue, longest_customer_monologue, user_talk_percentage, user, customer):
     JSON_TEMPLATE["longest_user_monologue"] = longest_user_monologue
@@ -81,6 +83,7 @@ def populated_json(longest_user_monologue, longest_customer_monologue, user_talk
     JSON_TEMPLATE["customer"] = customer
     json_string = json.dumps(JSON_TEMPLATE)
     print(json_string)
+
 
 def init():
     user_data = get_channel_data(CUSTOMER_DATAFILE)
@@ -92,7 +95,8 @@ def init():
     call_duration = (get_talk_duration(user_data, customer_data))
     user_talk_percentage = get_channel_talk_percentage(user_data["total_talk_time"], call_duration)
 
-    return populated_json(longest_user_monologue, longest_customer_monologue, user_talk_percentage, user_data["talks"], customer_data["talks"])
+    return populated_json(longest_user_monologue, longest_customer_monologue, user_talk_percentage, user_data["talks"],
+                          customer_data["talks"])
 
 
 init()
